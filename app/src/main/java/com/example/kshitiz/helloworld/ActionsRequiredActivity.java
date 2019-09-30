a917f84de7006f2345c4445d37d302aacaa8979f
tantrumkiller <joshikshitij_13@yahoo.in>
2019-09-29 20:43:13 -0700

    fix view

--- app/src/main/java/com/example/kshitiz/helloworld/ActionsRequiredActivity.java
+++ app/src/main/java/com/example/kshitiz/helloworld/ActionsRequiredActivity.java
@@ -5,9 +5,15 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
 
 import com.example.kshitiz.helloworld.setting.Setting;
 import com.example.kshitiz.helloworld.setting.SettingsConfig;
@@ -17,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class ActionsRequiredActivity extends Activity {
+public class ActionsRequiredActivity extends AppCompatActivity {
     private final UserPreferencesStore userPreferencesStore;
     private final SettingsConfig settingConfiguration;
 
@@ -32,6 +38,34 @@ public class ActionsRequiredActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_actions_required);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        populateList();
+
+        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.swiperefresh);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                populateList(); // your code
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    private void populateList(){
         final RecyclerView recyclerView = findViewById(R.id.actions_requried_recycler_view);
 
         // use this setting to improve performance if you know that changes
@@ -56,6 +90,5 @@ public class ActionsRequiredActivity extends Activity {
         recyclerView.setAdapter(mAdapter);
         recyclerView.addItemDecoration(
                 new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
-
     }
 }
